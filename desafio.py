0eef964a5e4ed5aba663304761223091ee7a900d
zaiaaa <gustavo.0801214@gmail.com>
2023-07-11 01:03:31 -0300

    toda parte de saque e extrato

--- desafio.py
+++ desafio.py
@@ -1,8 +1,10 @@
 import os
 import time as t
 
+
 def limpar_console():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 saldo = 0
 
@@ -20,21 +22,60 @@ menu = """
 """
 
 while True:
-    #print(menu)
-    
+    # print(menu)
+
     opcao = int(input(menu))
 
     if opcao == 0:
         print("Quanto deseja sacar?")
-        saque = int(input())
-        print(f"Você sacou: {saque}")
-        LIMITE_SAQUE_DIARIO -= 1
-        t.sleep(3)
-
-
-        print(LIMITE_SAQUE_DIARIO)
+        saque = float(input())
+        if saque > LIMITE_SAQUE:
+            print("Você não pode sacar mais que R$ 500.00")
+            print("aguarde...")
+            t.sleep(2)
+        elif saque > saldo:
+            print(f"Saldo insuficiente. (Saldo atual: {saldo: .2f})")
+            print("Aguarde..")
+            t.sleep(2)
+        elif LIMITE_SAQUE_DIARIO == 0:
+            print("Você atingiu o limite de saques diários!")
+            print("aguarde...")
+            t.sleep(2)
+        elif saque < 0:
+            print("Você não pode depositar um valor negativo.")
+        else:
+            print(f"Você sacou: R${saque: .2f}")
+            LIMITE_SAQUE_DIARIO -= 1
+            qtd_saque += 1
+            saldo -= saque
+            print("aguarde...")
+            t.sleep(2)
     elif opcao == 1:
         print("Quanto deseja depositar?")
-    
-    
+        depositou = float(input("R$ "))
+        if depositou < 0:
+            print("Você não pode depositar um valor negativo")
+            print("Aguarde..")
+            t.sleep(2)
+        else:
+            print(f"Você depositou: R${depositou: .2f}")
+            saldo += depositou
+            qtd_deposito += 1
+            print("Aguarde...")
+            t.sleep(2)
+    elif opcao == 2:
+        print(f"""
+===Extrato===
+Quantidade de saques restante = {LIMITE_SAQUE_DIARIO}
+Saques realizador = {qtd_saque}
+Depósitos realizados = {qtd_deposito}
+
+saldo = {saldo}
+
+
+        """)
+        t.sleep(5)
+    elif opcao == 3:
+        break
+
     limpar_console()
